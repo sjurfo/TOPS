bca41401c5a087da50aea86ab580432e9702c4da
Sjur Føyen <foyen.sjur@ntnu.no>
2025-09-25 09:00:26 +0200

    Added empty class for GEN_VSCDroop

--- src/dyn_models/gen.py
+++ src/dyn_models/gen.py
@@ -2,6 +2,52 @@ import numpy as np
 from src.dyn_models.utils import DAEModel
 import src.utility_functions as dps_uf
 
+
+class GEN_VSCDroop(DAEModel):
+    """
+    The synchronous machine classical + voltage control model, equivalent equations to a droop-controlled VSC.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for req_attr, default in zip(['PF_n', 'N_par', 'R', 'X_l'], [1, 1, 0, 0]):
+            if not req_attr in self.par.dtype.names:
+                new_field = np.ones(len(self.par), dtype=[(req_attr, float)])
+                new_field[req_attr] *= default
+                self.par = dps_uf.combine_recarrays(self.par, new_field)
+
+        fix_idx = self.par['V_n'] == 0
+        gen_bus_idx = dps_uf.lookup_strings(self.par['bus'], self.sys_par['bus_names'])
+        self.par['V_n'][fix_idx] = self.sys_par['bus_v_n'][gen_bus_idx][fix_idx]
+
+        fix_idx = self.par['S_n'] == 0
+        self.par['S_n'][fix_idx] = self.sys_par['s_n']
+
+        self.bus_idx = np.array(np.zeros(self.n_units), dtype=[(key, int) for key in self.bus_ref_spec().keys()])
+        self.bus_idx_red = np.array(np.zeros(self.n_units), dtype=[(key, int) for key in self.bus_ref_spec().keys()])
+
+    def bus_ref_spec(self):
+        return {'terminal': self.par['bus']}
+    def load_flow_pv(self):
+        return self.bus_idx['terminal'], -self.par['P']*self.par['N_par'], self.par['V']
+    def init_from_load_flow(self, x_0, v_0, S):
+        pass
+    def dyn_const_adm(self):
+        pass
+
+    def state_list(self):
+        pass
+
+    def input_list(self):
+        pass
+
+    def current_injections(self, x, v):
+        pass
+
+    def state_derivatives(self, dx, x, v):
+        pass
+
+
 class GEN(DAEModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -219,73 +265,3 @@ class GEN(DAEModel):
         # Nominal active power (can be used in governor models)
         PF_n = self.par['cosphi_n'] if 'cosphi_n' in self.par.dtype.names else 1
         return self.par['S_n']*PF_n
-
-
-    # def initialize(self, x0, v0, output_0):
-    #     p = self.par
-    #     X0 = self.local_view(x0)
-    #     # print(X0['speed'])
-    #     v_g = v0[self.bus_idx_red['terminal']]
-    #
-    #     S_g = output_0['P_e'] + 1j * output_0['Q']
-    #     PF_n = p['PF_n'] if 'PF_n' in p.dtype.names else 1
-    #     P_m_0 = S_g.real / PF_n
-    #
-    #     I_g = np.conj(S_g / v_g)
-    #     # output['I_g_abs'][:] = np.abs(I_g)
-    #     # output['I_g_angle'][:] = np.angle(I_g)
-    #
-    #     e_q_tmp = v_g + 1j * p['X_q'] * I_g
-    #     angle = np.angle(e_q_tmp)
-    #     speed = np.zeros_like(angle)
-    #
-    #     d = np.exp(1j * (angle - np.pi / 2))
-    #     q = np.exp(1j * angle)
-    #
-    #     I_g_dq = I_g * np.exp(1j * (np.pi / 2 - angle))
-    #     I_d = I_g_dq.real
-    #     I_q = I_g_dq.imag  # q-axis leading d-axis
-    #
-    #     v_g_dq = v_g * np.exp(1j * (np.pi / 2 - angle))
-    #     v_d = v_g_dq.real
-    #     v_q = v_g_dq.imag
-    #
-    #     e_q_t = v_q + p['X_d_t'] * I_d
-    #     e_d_t = v_d - p['X_q_t'] * I_q
-    #     e_t = e_q_t * q + e_d_t * d
-    #
-    #     e_q_st = v_q + p['X_d_st'] * I_d
-    #     e_d_st = v_d - p['X_q_st'] * I_q
-    #     e_st = e_q_st * q + e_d_st * d
-    #
-    #     e_q = e_q_t + I_d * (p['X_d'] - p['X_d_t'])
-    #     e = e_q * np.exp(1j * angle)
-    #     e_q_0 = e_q.copy()
-    #
-    #     P_m = P_m_0.copy()
-    #     P_e = e_q_st * I_q + e_d_st * I_d - (p['X_d_st'] - p['X_q_st']) * I_d * I_q
-    #     X0['speed'][:] = speed
-    #     X0['angle'][:] = angle
-    #     X0['e_q_t'][:] = e_q_t
-    #     X0['e_d_t'][:] = e_d_t
-    #     X0['e_q_st'][:] = e_q_st
-    #     X0['e_d_st'][:] = e_d_st
-
-    # def current_injections_new(self, x, v):
-    #     i_inj_d = self.e_q_st(x, v)/(1j*self.par['X_d_st'])*self.q(x, v)*self.par['N_par']
-    #     i_inj_q = self.e_d_st(x, v)/(1j*self.par['X_q_st'])*self.d(x, v)*self.par['N_par']
-    #     return i_inj_d, i_inj_q
-    #
-    # def state_derivatives_new(self, dx, x, v):
-    #
-    #     # Generators
-    #     T_m = input['P_m'] / (1 + x['speed'])
-    #
-    #     H = self.par['H']/self.par['PF_n']
-    #
-    #     dx['speed'][:] = 1/(2*H)*(T_m - self.p_e(x, v) - self.par['D'] * self.speed(x, v))
-    #     dx['angle'][:] = self.speed(x, v)*2*np.pi*self.int_par['f']
-    #     dx['e_q_t'][:] = 1/(self.par['T_d0_t'])*(input['E_f'] + input['v_aux'] - self.e_q_t(x, v) - self.i_d(x, v) * (self.par['X_d'] - self.par['X_d_t']))
-    #     dx['e_d_t'][:] = 1 / (self.par['T_q0_t']) * (-self.e_d_t(x, v) + self.i_q(x, v) * (self.par['X_q'] - self.par['X_q_t']))
-    #     dx['e_q_st'][:] = 1 / (self.par['T_d0_st']) * (self.e_q_t(x, v) - self.e_q_st(x, v) - self.i_d(x, v) * (self.par['X_d_t'] - self.par['X_d_st']))
-    #     dx['e_d_st'][:] = 1 / (self.par['T_q0_st']) * (self.e_d_t(x, v) - self.e_d_st(x, v) + self.i_q(x, v) * (self.par['X_q_t'] - self.par['X_q_st']))
